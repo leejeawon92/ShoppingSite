d2c3f2c8b3182b8b49b5d5689ae2343b02c73cd7
leejeawon92 <jeawon33333@gmail.com>
2023-11-11 00:06:54 +0900

    navbar layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   );
 }
 
