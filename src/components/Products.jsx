6684e44c8766eed5727a59729efbf418fb07e173
leejeawon92 <jeawon33333@gmail.com>
2023-11-26 00:00:59 +0900

    Go to the product details page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { getProducts } from '../api/firebase';
+import ProductCard from './ProductCard';
 
 export default function Products() {
   const { isLoading, error, data: products } = useQuery({ queryKey: ['products'], queryFn: getProducts });
@@ -12,14 +13,7 @@ export default function Products() {
       <ul className='grid grid-cols-1 md:grid-cols-3 lg-grid-cols-4 gap-4 p-4'>
         {products &&
           products.map((product) => (
-            <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
-            <img className='w-full' src={product.image} alt={product.title} />
-            <div className='mt-2 px-2 text-lg flex justify-between items-center'>
-              <h3 className='truncate'>{product.title}</h3>
-              <p>{`₩${product.price}`}</p>
-            </div>
-            <p className='mb-2 px-2 text-gray-600'>{product.category}</p>
-          </li>
+            <ProductCard key={product.id} product={product} />
           ))}
       </ul>
     </>
