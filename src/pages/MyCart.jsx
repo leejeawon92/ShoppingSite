6b37ede53d07f06085c1339a73b012c2adc125fc
leejeawon92 <jeawon33333@gmail.com>
2023-12-01 00:09:32 +0900

    Calculate the total amount of shopping cart

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { getCart } from '../api/firebase';
 import { useAuthContext } from '../context/AuthContext';
 import { useQuery } from '@tanstack/react-query';
+import PriceCard from '../components/PriceCard';
+import Button from '../components/ui/Button';
+import { BsFillPlusCircleFill } from 'react-icons/bs';
+import { FaEquals } from 'react-icons/fa';
+
+const SHIPPING = 3000;
 
 export default function MyCart() {
   const { uid } = useAuthContext();
@@ -10,6 +16,8 @@ export default function MyCart() {
   if (isLoading) return <p>Loading...</p>;
 
   const hasProducts = products && products.length > 0;
+
+  const totalPrice = products && products.reduce((prev, current) => prev + parseInt(current.price) * current.quantity, 0);
 
   return (
     <section className='p-8 flex flex-col'>
@@ -34,6 +42,14 @@ export default function MyCart() {
                 </li>
               ))}
           </ul>
+          <div className='flex justify-between items-center mb-6 px-2 md:px-8 lg:px-16'>
+            <PriceCard text='상품 총액' price={totalPrice} />
+            <BsFillPlusCircleFill className='shrink-0' />
+            <PriceCard text='배송액' price={SHIPPING} />
+            <FaEquals className='shrink-0' />
+            <PriceCard text='총가격' price={totalPrice + SHIPPING} />
+          </div>
+          <Button text='주문하기' />
         </>
       )}
     </section>
