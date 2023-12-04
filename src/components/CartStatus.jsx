a81d2bc60eb096772a1dd62d45b77b205468eea1
leejeawon92 <jeawon33333@gmail.com>
2023-12-05 00:08:34 +0900

    Custom hook for cartAPI

--- src/components/CartStatus.jsx
+++ src/components/CartStatus.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
-import { getCart } from '../api/firebase';
-import { useQuery } from '@tanstack/react-query';
-import { useAuthContext } from '../context/AuthContext';
 
 export default function CartStatus() {
-  const { uid } = useAuthContext();
-  const { data: products } = useQuery(['carts'], () => getCart(uid));
+  const { cartQuery: { data: products }} = useCart();
 
   return (
     <div className='relative'>
